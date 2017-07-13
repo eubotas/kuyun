0d1c4269771959c642260de2ef8bd5c7e934448c
bshi <shi.bob@qq.com>
2017-07-13 14:58:33 +0800

    cross filter

---
+++ kuyun-common/src/main/java/com/kuyun/common/interceptor/CrossDomainInterceptor.java
@@ -0,0 +1,20 @@
+package com.kuyun.common.interceptor;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 允许跨域请求
+ */
+public class CrossDomainInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT");
+        //return super.preHandle(request, response, handler);
+        return !request.getMethod().equals("OPTIONS");
+    }
+}
