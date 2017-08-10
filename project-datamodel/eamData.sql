8a4a460d0e1ac1fc6222fc801cab91d65fd95a93
bshi <shi.bob@qq.com>
2017-08-10 10:18:48 +0800

    change pachage name

--- project-datamodel/eamData.sql
+++ project-datamodel/eamData.sql
@@ -120,7 +120,7 @@ INSERT INTO `eam_ticket_type` VALUES (11,'警报工单',1,'2017-07-26 15:51:51',
 # end prepare ticket menu item
 
 DELETE FROM `eam_protocol`;
-INSERT INTO `eam_protocol` VALUES (1, 'Modbus RTU', '127.0.0.1', 8234);
-INSERT INTO `eam_protocol` VALUES (2, 'Modbus TCP', '127.0.0.1', 8233);
-INSERT INTO `eam_protocol` VALUES (3, 'MQTT', '127.0.0.1', 8232);
+INSERT INTO `eam_protocol` VALUES (1, 'Modbus RTU', '118.89.140.11', 8234);
+INSERT INTO `eam_protocol` VALUES (2, 'Modbus TCP', '118.89.140.11', 8233);
+INSERT INTO `eam_protocol` VALUES (3, 'MQTT', '118.89.140.11', 8232);
 
--- project-datamodel/modelUpdate.sql
+++ project-datamodel/modelUpdate.sql
@@ -7,3 +7,4 @@ ALTER TABLE upms_system modify name varchar(50);
 
 ALTER TABLE eam_equipment ADD   province    varchar(10);
 ALTER TABLE eam_equipment ADD   city        varchar(10);
+ALTER TABLE eam_equipment_model_properties ADD   display_type        varchar(10);
